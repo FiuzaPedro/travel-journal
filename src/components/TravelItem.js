52ddab41906ce23eb7f80760ab3c565055095e3a
Pedro Fiuza <pedro_fiuza@hotmail.com>
2023-04-03 10:16:29 +0100

    Added thumbnails and picture changing event

--- src/components/TravelItem.js
+++ src/components/TravelItem.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-
+import images from '../data/images';
 
 export default function TravelItem(props) {
-    const location = <FontAwesomeIcon icon={faLocationDot} />;
+    function changeImg(e) {
+        let currentThumbClicked = e.target.alt.split(' ')[0];
+        let currentImg = document.getElementById(currentThumbClicked);
+        currentImg.style.opacity = '0';
+        setTimeout(() => {
+            currentImg.setAttribute('src', e.target.src);
+            currentImg.style.opacity = '1';
+        }, 500);
 
+    }
+    const location = <FontAwesomeIcon icon={faLocationDot} />;
+    const imagesInfo = images.map((value) => {
+        return (
+            <div className='thumbnailsContainer'>
+                {value.title === props.title ? <img className='imgThumb' src={value.imageSrc} alt={value.title} onClick={changeImg} /> : ''}
+            </div>
+        )
+    })
     return (
         <div className="divTravelItem">
-            <div className="imgContainer"><img src={props.imageUrl} alt={props.title} /></div>
+            <div className="imgContainer"><img className='imgLarge' id={props.title.split(' ')[0]} src={props.imageUrl} alt={props.title} /></div>
             <div className="infoContainer">
                 <p>
                     <span className='spanLocation'>{location} {props.location}</span> &nbsp;
@@ -17,7 +33,10 @@ export default function TravelItem(props) {
                 <h1>{props.title}</h1>
                 <h3>{props.startDate} - {props.endDate}</h3>
                 <p>{props.description}</p>
+
+                {imagesInfo}
             </div>
+
         </div>
     );
 }
---
+++ src/data/images.js
@@ -0,0 +1,47 @@
+const images = [
+    {
+        title: "Stonehenge",
+        imageSrc: "../images/stonehenge.jpg"
+    },
+    {
+        title: "Stonehenge",
+        imageSrc: "../images/stonehenge2.jpg"
+    },
+    {
+        title: "Stonehenge",
+        imageSrc: "https://static.nationalgeographic.de/files/styles/image_3200/public/ankit-sood-q0ZLK_D7ngI-unsplash.jpg?w=1900&h=1267"
+    },
+    {
+        title: "Stonehenge",
+        imageSrc: "https://cdn.prod.www.spiegel.de/images/16114f23-be50-4ace-8b95-1e302f63cce9_w1600_r1.5001339405304046_fpx50_fpy54.jpg"
+    },
+    {
+        title: "Lagoa das 7 Cidades",
+        imageSrc: "https://byacores.com/wp-content/uploads/2017/10/IMG_8426.jpg"
+    },
+    {
+        title: "Lagoa das 7 Cidades",
+        imageSrc: "https://byacores.com/wp-content/uploads/2017/09/11057631_10204684701023966_8745752039488519647_o.jpg"
+    },
+    {
+        title: "Lagoa das 7 Cidades",
+        imageSrc: "https://i0.wp.com/www.futurismo.pt/blog/wp-content/uploads/2022/12/Alexandre-Balas-Sete-Cidades1.jpg?resize=819%2C1024&ssl=1"
+    },
+    {
+        title: "Lagoa das 7 Cidades",
+        imageSrc: "https://mediaim.expedia.com/destination/1/0335360dd3359af829fe7a070f7139a9.jpg"
+    },
+    {
+        title: "Sunset Beach",
+        imageSrc: "https://ml0i9otuup1b.i.optimole.com/AmM8BM4-5NyDhkmh/w:665/h:374/q:mauto/f:avif/https://www.aloha-hawaii.com/wp-content/uploads/2010/07/sunset-beach.jpg"
+    },
+    {
+        title: "Sunset Beach",
+        imageSrc: "https://www.best-of-oahu.com/images/Sunset-Beach-Oahu-Sunset.jpg"
+    },
+    {
+        title: "Sunset Beach",
+        imageSrc: "https://images.fineartamerica.com/images-medium-large-5/sunset-beach-north-shore-oahu-hawaii-michael-defreitas.jpg"
+    }
+]
+export default images;
